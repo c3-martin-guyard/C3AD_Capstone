33e43774c860e126f4f7effd44860e685eb0ec55
Martin Guyard <Martin.Guyard@c3.ai>
2022-04-26 15:03:13 +0200

    New energyUsageBulbWindow and member function

--- c3training/lightbulbEnergy/src/entity/WeeklyEnergyUsage.js
+++ c3training/lightbulbEnergy/src/entity/WeeklyEnergyUsage.js
@@ -11,3 +11,17 @@ function energyUsageMeter(endDate) {
     weeklyUsageSum = metricOutput.result[this.id].EnergyUsageMeterWindow.data()[0];
     return weeklyUsageSum;
 }
+
+function energyUsageBulb(endDate) {
+    var metricSpec, weeklyUsageSum, metricOutput;
+    metricSpec = EvalMetricsSpec.make({
+        ids: [this.id],
+        expressions: ["EnergyUsageBulbWindow"],
+        start: endDate,
+        end: endDate,
+        interval: "DAY"
+    });
+    metricOutput = this.type().evalMetrics(metricSpec);
+    weeklyUsageSum = metricOutput.result[this.id].EnergyUsageBulbWindow.data()[0];
+    return weeklyUsageSum;
+}
